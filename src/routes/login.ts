a04900686b93b0bb81c273c93dc07a736fe013f9
Millzy <millzyyt08@gmail.com>
2023-11-06 13:31:19 +0000

    fix login route

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 export const loginRoute = Router();
 
 loginRoute.get("/login", (req: Request, res: Response) => {
-    res.render("index");
+    res.render("login");
 });
 
 loginRoute.post("/login", (req: Request, res: Response) => {

7dfddf161331b6a502127686aed6aad4cf63e886
Millzy <millzyyt08@gmail.com>
2023-11-06 12:42:35 +0000

    user if system

--- src/schemas/userDayInfo.ts
+++ src/schemas/userDayInfo.ts
@@ -1,6 +1,7 @@
 import { Schema } from "mongoose";
+import { UserDayInfoStatus } from "./enum/userDayInfoStatus";
 
 export const userDayInfoSchema: Schema = new Schema({
-    user: User,
-
+    user: Number,
+    status: UserDayInfoStatus
 })

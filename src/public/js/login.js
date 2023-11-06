4c398946ea2f8a76b0ff968b6ff30ef0d3c6d2ae
Millzy <millzyyt08@gmail.com>
2023-11-06 13:31:51 +0000

    get token from db

---
+++ src/public/js/login.js
@@ -0,0 +1,27 @@
+const { response } = require("express");
+
+const usernameInput = document.getElementById("username-input");
+const passwordInput = document.getElementById("password-input");
+
+async function getToken(username, password) {
+    const response = await fetch("/login", {
+        method: "POST",
+        body: JSON.stringify({
+            user: username,
+            pass: password
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+
+    if (!response.ok)
+        return null;
+
+    const json = await response.json();
+    return json.token;
+}
+
+function login() {
+    
+}

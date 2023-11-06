aa42dd5eb58f22515423e3af35a9fe6e50111cb5
Millzy <millzyyt08@gmail.com>
2023-11-06 12:42:23 +0000

    add subdocument array

--- src/schemas/calendarDay.ts
+++ src/schemas/calendarDay.ts
@@ -1,6 +1,7 @@
 import { Schema } from 'mongoose';
+import { userDayInfoSchema } from './userDayInfo';
 
 export const calendarDaySchema: Schema = new Schema({
     date: String,
-
+    userInfos: [userDayInfoSchema]
 });
